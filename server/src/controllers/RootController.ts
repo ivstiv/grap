040de6e43c4af7b0c07dcb1580b0f7eec610f24f
ivstiv <ivstiv@users.noreply.github.com>
2022-06-12 14:26:18 +0100

    Getting some content written for the front pages

--- server/src/controllers/RootController.ts
+++ server/src/controllers/RootController.ts
@@ -6,3 +6,10 @@ export const index: FastifyHandler =
     res.view("/src/views/pages/index.ejs", {
       isLoggedIn: !!req.session.user,
     });
+
+
+export const about: FastifyHandler = 
+    async (req, res) =>
+      res.view("/src/views/pages/about.ejs", {
+        isLoggedIn: !!req.session.user,
+      });
--- server/src/routes/root.ts
+++ server/src/routes/root.ts
@@ -5,5 +5,6 @@ import * as RootController from "../controllers/RootController";
 export const rootRoutes: FastifyPluginCallback =
   (instance, _opts, next) => {
     instance.get("/", RootController.index);
+    instance.get("/about", RootController.about);
     next();
   };
