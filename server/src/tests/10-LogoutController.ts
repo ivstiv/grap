f33471fd862c00c9898c0429573157bbf53a0e98
ivstiv <25481564+ivstiv@users.noreply.github.com>
2022-07-22 00:19:23 +0300

    Testing edge cases (#17)

---
+++ server/src/tests/10-LogoutController.ts
@@ -0,0 +1,87 @@
+import assert from "assert";
+import { afterEach } from "mocha";
+import { User } from "../models/User";
+import { webServer } from "../web-server";
+import { Cookie, systemCleanup } from "./utils";
+
+describe("Logout routes", () => {
+  let user: User;
+
+  beforeEach(async () => {
+    const registered = await User.register("user@grap.email", "123456");
+    if (!registered) {
+      throw new Error("Failed to register user.");
+    }
+    user = registered;
+  });
+
+  afterEach(async () => {
+    user = undefined as unknown as User;
+    await systemCleanup();
+  });
+
+
+  describe("GET /logout", () => {
+    it("Should redirect to login", async () => {
+      const res = await webServer.inject({
+        method: "GET",
+        url: "/logout",
+      });
+
+      const redirectLocation = res.headers["location"];
+      assert.strictEqual(res.statusCode, 302);
+      assert.strictEqual(redirectLocation, "/login");
+    });
+
+
+    it("Should destroy session and redirect to logout", async () => {
+      const loginRes = await webServer.inject({
+        method: "POST",
+        url: "/login",
+        payload: {
+          email: user.email,
+          password: "123456",
+        },
+      });
+
+      const sessionCookie = loginRes.cookies.find(c =>
+        (c as Cookie).name === "sessionId"
+      ) as Cookie;
+
+      const dashboardResBeforeLogout = await webServer.inject({
+        method: "GET",
+        url: "/dashboard",
+        cookies: {
+          [sessionCookie.name]: `${sessionCookie.value}`,
+        },
+      });
+
+      assert.strictEqual(dashboardResBeforeLogout.statusCode, 200);
+
+      const res = await webServer.inject({
+        method: "GET",
+        url: "/logout",
+        cookies: {
+          [sessionCookie.name]: `${sessionCookie.value}`,
+        },
+      });
+
+      const redirectLocation = res.headers["location"];
+      assert.strictEqual(res.statusCode, 302);
+      assert.strictEqual(redirectLocation, "/login");
+
+
+      const dashboardResAfterLogout = await webServer.inject({
+        method: "GET",
+        url: "/dashboard",
+        cookies: {
+          [sessionCookie.name]: `${sessionCookie.value}`,
+        },
+      });
+
+      const redirectLocationAfterLogout = dashboardResAfterLogout.headers["location"];
+      assert.strictEqual(dashboardResAfterLogout.statusCode, 302);
+      assert.strictEqual(redirectLocationAfterLogout, "/login");
+    });
+  });
+});
